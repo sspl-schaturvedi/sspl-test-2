d492e2640ea0ab5b45ad28ac4619a4feefe495a6
sspl-schaturvedi <schaturvedi@stepoutsolutions.com>
2015-06-10 13:13:50 +0530

    Updated database credentials

--- v2/config.php
+++ v2/config.php
@@ -3,8 +3,8 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-$_SESSION['db_user'] = 'stepouts_hul';
-$_SESSION['db_pass'] = '[vQ#D}g{V%bQ';
-$_SESSION['db_server'] = 'localhost';
-$_SESSION['db_name'] = 'stepouts_hulSocialTrends';
+$_SESSION['db_user'] = 'b3b856772cb6d0';
+$_SESSION['db_pass'] = '9098fb21';
+$_SESSION['db_server'] = 'ap-cdbr-azure-southeast-a.cloudapp.net';
+$_SESSION['db_name'] = 'db_sspl_test_2';
 ?>
